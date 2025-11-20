0de4dd97240b35b6f47b647b1ff25d4106d9a1ab
kalicout <sapatri2410@gmail.com>
2025-11-20 11:05:47 +0100

    Added shared routes , fixed ts config

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,20 @@
-import { offers } from "@/constants";
+import CartButton from "@/components/CartButton";
+import { images, offers } from "@/constants";
 import cn from "clsx";
 import { Fragment } from "react";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "./global.css";
 export default function App() {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-white">
       <FlatList
         data={offers}
         renderItem={({ item, index }: any) => {
@@ -19,6 +27,8 @@ export default function App() {
                   isEven ? "flex-row-reverse" : "flex-row"
                 )}
                 style={{ backgroundColor: item.color }}
+                //Androids only
+                android_ripple={{ color: "#ffffff22" }}
               >
                 {({ pressed }) => (
                   <Fragment>
@@ -29,8 +39,21 @@ export default function App() {
                         resizeMode={"contain"}
                       />
                     </View>
-                    <View className="offer-card__info">
-                      <Text>{item.title}</Text>
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isEven ? "pl-10" : "pr-10"
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor="#ffffff"
+                      />
                     </View>
                   </Fragment>
                 )}
@@ -38,6 +61,24 @@ export default function App() {
             </View>
           );
         }}
+        contentContainerClassName="pb-28 px-5"
+        // Dont use Flatlists inside scrollviews
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5 px-5">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">DELIVER TO</Text>
+              <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100">Spain</Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+            <CartButton />
+          </View>
+        )}
       />
     </SafeAreaView>
   );
